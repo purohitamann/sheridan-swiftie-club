3a843fb552847e0ee0a5f0c891f897b7562607c0
Aman Purohit <purohiam@sheridancollege.ca>
2025-02-27 23:37:50 -0500

    Remove unused Swiftville pages and components to streamline the application

--- src/app/so-high-school/page.tsx
+++ src/app/so-high-school/page.tsx
@@ -1,29 +1,29 @@
-'use client';
-import React, { useEffect } from 'react';
+// 'use client';
+// import React, { useEffect } from 'react';
 
-import Header from '@/components/Header/Header';
+// import Header from '@/components/Header/Header';
 
-import Body from '@/components/Body';
-import ThemeWrapper from '@/components/ThemeWrapper/ThemeWrapper';
+// import Body from '@/components/Body';
+// import ThemeWrapper from '@/components/ThemeWrapper/ThemeWrapper';
 
-const Home: React.FC = () => {
-  useEffect(() => {
+// const Home: React.FC = () => {
+//   useEffect(() => {
 
-    const audio = new Audio('/so-high-school-intro.mp3');
-    audio.volume = 0.5;
-    audio.play();
-  }, []);
+//     const audio = new Audio('/so-high-school-intro.mp3');
+//     audio.volume = 0.5;
+//     audio.play();
+//   }, []);
 
-  return (
-    <main className="flex flex-col min-h-screen">
+//   return (
+//     <main className="flex flex-col min-h-screen">
 
-      <ThemeWrapper>    <Header /></ThemeWrapper>
+//       <ThemeWrapper>    <Header /></ThemeWrapper>
 
-      <Body />
+//       <Body />
 
 
-    </main>
-  );
-};
+//     </main>
+//   );
+// };
 
-export default Home;
+// export default Home;
